64e33256418662686044c88b57f7296bafdc692f
dantran <dantran@cmc.edu>
2025-04-11 10:10:28 -0700

    fixed versions again

--- load_tweets.py
+++ load_tweets.py
@@ -103,7 +103,6 @@ def insert_tweet(connection,tweet):
 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
-    connection.commit()
     with connection.begin() as trans:
 
         ########################################

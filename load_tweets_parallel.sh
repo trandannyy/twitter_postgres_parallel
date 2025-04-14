ef1c3e622393bc311fba34a37ded8cb645f92fe0
dantran <dantran@cmc.edu>
2025-04-14 11:06:45 -0700

    pass tests locally

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,16 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:10257 --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:10258/ --inputs
